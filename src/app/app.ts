3fdca38ed075422799aac4c45cd25aa1f7a0eac2
DmitryMal <lotyry5@gmail.com>
2025-07-11 22:07:33 +0300

    Initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,39 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css']
+})
+export class CounterComponent {
+  count = 0;
+
+  increment() {
+    this.count++;
+  }
+
+  decrement() {
+    this.count--;
+  }
+
+  reset() {
+    this.count = 0;
+  }
+}
+
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class AppComponent {
+  title = 'hello';
+}
+export class App {
+  protected readonly title = signal('counter-app');
+}
+
+
