5050cf0f16f7b14a9e74090918413e4637e086f3
DmitryMal <lotyry5@gmail.com>
2025-07-11 23:05:02 +0300

    add routing and details component with route param

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'list', component: ListComponent },
+  { path: 'details/:id', component: DetailsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // по умолчанию
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
